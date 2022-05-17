fe0c70b1deb78a9ca7757f61c9cc4bd50b680a2a
Andy Caseres <andycaseres2@gmail.com>
2022-05-17 14:42:57 -0500

    modific skills

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
-import resume from '../assets/pdf/resume.pdf'
+import resume from "../assets/pdf/resume.pdf";
 
 export const headerData = {
-  name: 'Andy Caseres',
-  title: 'Web Developer',
+  name: "Andy Caseres",
+  title: "Frontend Developer Junior",
   desciption:
     "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-  image: 'https://i.ibb.co/z4ZYCzh/IMG-1748.jpg',
+  image: "https://i.ibb.co/z4ZYCzh/IMG-1748.jpg",
   resumePdf: resume,
-}
+};

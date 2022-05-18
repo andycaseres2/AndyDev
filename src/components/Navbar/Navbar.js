3b10dabb5ad7f9d28b4eafec696fd0194ca39866
Andy Caseres <andycaseres2@gmail.com>
2022-05-17 23:06:00 -0500

    correction navbar link projects

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -194,10 +194,10 @@ function Navbar() {
             </Fade>
 
             <Fade left>
-              <NavLink to="/#resume" smooth={true} spy="true" duration={2000}>
+              <NavLink to="/#projects" smooth={true} spy="true" duration={2000}>
                 <div className={classes.drawerItem}>
                   <HiDocumentText className={classes.drawerIcon} />
-                  <span className={classes.drawerLinks}>Resume</span>
+                  <span className={classes.drawerLinks}>Projects</span>
                 </div>
               </NavLink>
             </Fade>

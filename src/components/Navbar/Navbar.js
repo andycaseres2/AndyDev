3b4a1635664021db5fe892733e3e5636888d4268
Andy Caseres <andycaseres2@gmail.com>
2022-05-17 23:00:42 -0500

    delete links navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,134 +1,134 @@
-import React, { useContext, useState } from "react";
-import { NavHashLink as NavLink } from "react-router-hash-link";
-import Fade from "react-reveal/Fade";
-import { IoMenuSharp, IoHomeSharp } from "react-icons/io5";
-import { HiDocumentText } from "react-icons/hi";
-import { BsFillGearFill } from "react-icons/bs";
-import { MdPhone } from "react-icons/md";
-import { FaUser, FaFolderOpen } from "react-icons/fa";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CloseIcon from "@material-ui/icons/Close";
+import React, { useContext, useState } from 'react'
+import { NavHashLink as NavLink } from 'react-router-hash-link'
+import Fade from 'react-reveal/Fade'
+import { IoMenuSharp, IoHomeSharp } from 'react-icons/io5'
+import { HiDocumentText } from 'react-icons/hi'
+import { BsFillGearFill } from 'react-icons/bs'
+import { MdPhone } from 'react-icons/md'
+import { FaUser, FaFolderOpen } from 'react-icons/fa'
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import CloseIcon from '@material-ui/icons/Close'
 
-import "./Navbar.css";
-import { headerData } from "../../data/headerData";
-import { ThemeContext } from "../../contexts/ThemeContext";
+import './Navbar.css'
+import { headerData } from '../../data/headerData'
+import { ThemeContext } from '../../contexts/ThemeContext'
 
 function Navbar() {
-  const { theme, setHandleDrawer } = useContext(ThemeContext);
+  const { theme, setHandleDrawer } = useContext(ThemeContext)
 
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-    setHandleDrawer();
-  };
+    setOpen(true)
+    setHandleDrawer()
+  }
 
   const handleDrawerClose = () => {
-    setOpen(false);
-    setHandleDrawer();
-  };
+    setOpen(false)
+    setHandleDrawer()
+  }
 
   const useStyles = makeStyles((t) => ({
     navMenu: {
-      fontSize: "2.5rem",
+      fontSize: '2.5rem',
       color: theme.tertiary,
-      cursor: "pointer",
-      transform: "translateY(-10px)",
-      transition: "color 0.3s",
-      "&:hover": {
+      cursor: 'pointer',
+      transform: 'translateY(-10px)',
+      transition: 'color 0.3s',
+      '&:hover': {
         color: theme.primary,
       },
-      [t.breakpoints.down("sm")]: {
-        fontSize: "2.5rem",
+      [t.breakpoints.down('sm')]: {
+        fontSize: '2.5rem',
       },
-      [t.breakpoints.down("xs")]: {
-        fontSize: "2rem",
+      [t.breakpoints.down('xs')]: {
+        fontSize: '2rem',
       },
     },
     MuiDrawer: {
-      padding: "0em 1.8em",
-      width: "14em",
-      fontFamily: " var(--primaryFont)",
-      fontStyle: " normal",
-      fontWeight: " normal",
-      fontSize: " 24px",
+      padding: '0em 1.8em',
+      width: '14em',
+      fontFamily: ' var(--primaryFont)',
+      fontStyle: ' normal',
+      fontWeight: ' normal',
+      fontSize: ' 24px',
       background: theme.secondary,
-      overflow: "hidden",
-      borderTopRightRadius: "40px",
-      borderBottomRightRadius: "40px",
-      [t.breakpoints.down("sm")]: {
-        width: "12em",
+      overflow: 'hidden',
+      borderTopRightRadius: '40px',
+      borderBottomRightRadius: '40px',
+      [t.breakpoints.down('sm')]: {
+        width: '12em',
       },
     },
     closebtnIcon: {
-      fontSize: "2rem",
-      fontWeight: "bold",
-      cursor: "pointer",
+      fontSize: '2rem',
+      fontWeight: 'bold',
+      cursor: 'pointer',
       color: theme.primary,
-      position: "absolute",
+      position: 'absolute',
       right: 40,
       top: 40,
-      transition: "color 0.2s",
-      "&:hover": {
+      transition: 'color 0.2s',
+      '&:hover': {
         color: theme.tertiary,
       },
-      [t.breakpoints.down("sm")]: {
+      [t.breakpoints.down('sm')]: {
         right: 20,
         top: 20,
       },
     },
     drawerItem: {
-      margin: "2rem auto",
-      borderRadius: "78.8418px",
+      margin: '2rem auto',
+      borderRadius: '78.8418px',
       background: theme.secondary,
       color: theme.primary,
-      width: "85%",
-      height: "60px",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-evenly",
-      padding: "0 30px",
-      boxSizing: "border-box",
-      border: "2px solid",
+      width: '85%',
+      height: '60px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      padding: '0 30px',
+      boxSizing: 'border-box',
+      border: '2px solid',
       borderColor: theme.primary,
-      transition: "background-color 0.2s, color 0.2s",
-      "&:hover": {
+      transition: 'background-color 0.2s, color 0.2s',
+      '&:hover': {
         background: theme.primary,
         color: theme.secondary,
       },
-      [t.breakpoints.down("sm")]: {
-        width: "100%",
-        padding: "0 25px",
-        height: "55px",
+      [t.breakpoints.down('sm')]: {
+        width: '100%',
+        padding: '0 25px',
+        height: '55px',
       },
     },
     drawerLinks: {
-      fontFamily: "var(--primaryFont)",
-      width: "50%",
-      fontSize: "1.3rem",
+      fontFamily: 'var(--primaryFont)',
+      width: '50%',
+      fontSize: '1.3rem',
       fontWeight: 600,
-      [t.breakpoints.down("sm")]: {
-        fontSize: "1.125rem",
+      [t.breakpoints.down('sm')]: {
+        fontSize: '1.125rem',
       },
     },
     drawerIcon: {
-      fontSize: "1.6rem",
-      [t.breakpoints.down("sm")]: {
-        fontSize: "1.385rem",
+      fontSize: '1.6rem',
+      [t.breakpoints.down('sm')]: {
+        fontSize: '1.385rem',
       },
     },
-  }));
+  }))
 
-  const classes = useStyles();
+  const classes = useStyles()
 
   const shortname = (name) => {
     if (name.length > 12) {
-      return name.split(" ")[0];
+      return name.split(' ')[0]
     } else {
-      return name;
+      return name
     }
-  };
+  }
 
   return (
     <div className="navbar">
@@ -144,10 +144,10 @@ function Navbar() {
       <Drawer
         variant="temporary"
         onClose={(event, reason) => {
-          if (reason !== "backdropClick") {
-            handleDrawerClose();
-          } else if (reason !== "escapeKeyDown") {
-            handleDrawerClose();
+          if (reason !== 'backdropClick') {
+            handleDrawerClose()
+          } else if (reason !== 'escapeKeyDown') {
+            handleDrawerClose()
           }
         }}
         anchor="left"
@@ -160,9 +160,9 @@ function Navbar() {
           <CloseIcon
             onClick={handleDrawerClose}
             onKeyDown={(e) => {
-              if (e.key === " " || e.key === "Enter") {
-                e.preventDefault();
-                handleDrawerClose();
+              if (e.key === ' ' || e.key === 'Enter') {
+                e.preventDefault()
+                handleDrawerClose()
               }
             }}
             className={classes.closebtnIcon}
@@ -202,7 +202,7 @@ function Navbar() {
               </NavLink>
             </Fade>
 
-            <Fade left>
+            {/* <Fade left>
               <NavLink to="/#services" smooth={true} spy="true" duration={2000}>
                 <div className={classes.drawerItem}>
                   <BsFillGearFill className={classes.drawerIcon} />
@@ -218,7 +218,7 @@ function Navbar() {
                   <span className={classes.drawerLinks}>Blog</span>
                 </div>
               </NavLink>
-            </Fade>
+            </Fade> */}
 
             <Fade left>
               <NavLink to="/#contacts" smooth={true} spy="true" duration={2000}>
@@ -232,7 +232,7 @@ function Navbar() {
         </div>
       </Drawer>
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar

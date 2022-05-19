37ccea14fc88bb01e18a2435b1815fa444c9d893
Andy Caseres <andycaseres2@gmail.com>
2022-05-19 16:24:50 -0500

    add info projects and abount

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,8 @@
 export const aboutData = {
-  title: 'Who I am',
+  title: "Who I am",
   description1:
-    "My name's Andy. I'm a Frontend Developer Junior based in Bogota D.C, Colombia.",
+    "My name's Andy. I'm a Frontend Developer Junior based in Bogota, Colombia.",
   description2:
     "I am a young man passionate about programming, self-taught. I have a very good level of learning. I hope soon to fulfill my dream of working in the IT sector and thus give the best of myself and develop my skills and knowledge, fulfilling the team's objectives.",
   image: 1,
-}
+};

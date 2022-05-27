8dc46de08f52e358405ce98cbd5c9c576da6905c
Andy Caseres <andycaseres2@gmail.com>
2022-05-27 15:37:14 -0500

    modific header text

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,8 +3,13 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
   name: 'Andy Caseres',
   title: 'Frontend Developer Junior',
-  desciption:
-    'I am currently looking for Front End Junior positions. I characterize myself as a dedicated, proactive responsible and solid person to meet goals. Eager to be able to offer my knowledge in a work environment, and to be able to develop myself in a team.',
+  desciption:`
+  I am a frontend oriented self-taught web developer. Currently I 
+  have knowledge in Frontend and Studying some Backend Technologies
+   to become a Developer with the MERN Stack. I really want to learn
+    and be able to share my knowledge in a work environment and be able 
+    to develop as a team.
+    `,
   image: 'https://i.ibb.co/z4ZYCzh/IMG-1748.jpg',
   resumePdf: resume,
 }
